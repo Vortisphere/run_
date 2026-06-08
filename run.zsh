84ecddbabf1bcaf84d88f0e16699a4edf6d930d9
Vxid3d <vaishalirprasad2018@gmail.com>
2026-06-08 09:26:35 -0700

    Update run.zsh

--- run.zsh
+++ run.zsh
@@ -1,14 +1,31 @@
 #!/usr/bin/env zsh
 
 echo "Enter your command to execute it, "
-echo ""
+echo "-py to run python code," 
+echo "*py to run a python file,"
 echo "and exit to exit"
 while true; do
     read -r -e "input?>> "
     if [[ "$input" == "exit" ]]; then
         break
+    elif [[ "$input" == "-py" ]]; then
+        echo "Enter your Python code. Type: END on a new line when finished:"
+        code=""
+        while true; do
+            read -r line
+            if [[ "$line" == "END" ]]; then
+                break
+            fi
+            code+="$line"$'\n'
+        done
+        python3 -c "$code"
     elif [[ "$input" == "*py" ]]; then
-        python3 
+        read -r "filer?Enter your file name: "
+        if [[ ! -f "$filer" ]]; then
+            echo "The file is missing."
+        else
+            python3 "$filer"
+        fi
     else
         eval "$input"
     fi
