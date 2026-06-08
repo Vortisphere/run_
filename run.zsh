a122506de932f16746b0f2f97957eb04020cf797
vuf1 <vaishalirprasad2018+github@gmail.com>
2026-06-07 17:52:34 -0700

    Update run.zsh

--- run.zsh
+++ run.zsh
@@ -1,11 +1,14 @@
 #!/usr/bin/env zsh
 
 echo "Enter your command to execute it, "
+echo ""
 echo "and exit to exit"
 while true; do
     read -r -e "input?>> "
     if [[ "$input" == "exit" ]]; then
         break
+    elif [[ "$input" == "*py" ]]; then
+        python3 
     else
         eval "$input"
     fi
